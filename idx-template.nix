e7623ca8e48c812d021d74c780dd45349ec82deb
SparshKaushik <contact@sparshkaushik.com>
2024-08-11 12:47:53 +0000

    make sqlite default dbprovider and fix nextauth arg

--- idx-template.nix
+++ idx-template.nix
@@ -1,4 +1,4 @@
-{ pkgs, language ? "ts", tailwind ? false, trpc ? false, nextauth ? false, primsa ? false, drizzle ? false, git_init ? false, db} : {
+{ pkgs, language ? "ts", tailwind ? false, trpc ? false, nextauth ? false, primsa ? false, drizzle ? false, git_init ? false, db ? "sqlite"} : {
   packages = [
     pkgs.nodejs_20
     pkgs.bun
@@ -6,6 +6,6 @@
   bootstrap = ''
     mkdir -p "$out"
     cp -rf ${./.idx} "$out"
-    bun create t3-app@latest --CI ${if tailwind then "--tailwind" else ""} ${if trpc then "--trpc" else ""} ${if nextauth then "--nextauth" else ""} ${if primsa then "--primsa" else ""} ${if drizzle then "--drizzle" else ""} ${if git_init then "" else "--noGit"} --dbProvider ${db} "$out"
+    bun create t3-app@latest --CI ${if tailwind then "--tailwind" else ""} ${if trpc then "--trpc" else ""} ${if nextauth then "--nextAuth" else ""} ${if primsa then "--primsa" else ""} ${if drizzle then "--drizzle" else ""} ${if git_init then "" else "--noGit"} --dbProvider ${db} "$out"
   '';
 }
