4120b69b6959f6a5d67e9aa56bc0e724cf73054b
SparshKaushik <contact@sparshkaushik.com>
2024-08-11 12:46:08 +0000

    fix options and remove app router

--- idx-template.nix
+++ idx-template.nix
@@ -1,11 +1,11 @@
-{ pkgs, language ? "ts", tailwind ? false, trpc ? false, nextauth ? false, primsa ? false, drizzle ? false, approuter ? false, git_init ? false, db} : {
+{ pkgs, language ? "ts", tailwind ? false, trpc ? false, nextauth ? false, primsa ? false, drizzle ? false, git_init ? false, db} : {
   packages = [
     pkgs.nodejs_20
     pkgs.bun
   ];
   bootstrap = ''
     mkdir -p "$out"
-    bun create t3-app@latest --CI --tailwind ${tailwind} --trpc ${trpc} --nextauth ${nextauth} --primsa ${primsa} --drizzle ${drizzle} --approuter ${approuter} --git-init ${git_init} --db ${db} "$out"
     cp -rf ${./.idx} "$out"
+    bun create t3-app@latest --CI ${if tailwind then "--tailwind" else ""} ${if trpc then "--trpc" else ""} ${if nextauth then "--nextauth" else ""} ${if primsa then "--primsa" else ""} ${if drizzle then "--drizzle" else ""} ${if git_init then "" else "--noGit"} --dbProvider ${db} "$out"
   '';
 }
